a8c50812a288052a0558a54bde1ff3d5c9854a9f
jin57.li <jin57.li@tcl.com>
2025-01-22 21:45:34 +0800

    VPSS 调整

--- solutions/smart_doorbell/customization/turnkey_qfn/param/custom_sysparam.c
+++ solutions/smart_doorbell/customization/turnkey_qfn/param/custom_sysparam.c
@@ -10,9 +10,17 @@
 
 PARAM_CLASSDEFINE(PARAM_VB_CFG_S,VBPOOL,CTX,VB)[] = {
     {
-        .u16width = 1920,
-        .u16height = 1080,
+        .u16width = 2560,
+        .u16height = 1440,
         .u8VbBlkCnt = 7,
+        .fmt = PIXEL_FORMAT_NV21,
+        .enBitWidth = DATA_BITWIDTH_8,
+        .enCmpMode = COMPRESS_MODE_NONE,
+    },
+    {
+        .u16width = PANEL_WIDTH,
+        .u16height = PANEL_HEIGHT,
+        .u8VbBlkCnt = 3,
         .fmt = PIXEL_FORMAT_NV21,
         .enBitWidth = DATA_BITWIDTH_8,
         .enCmpMode = COMPRESS_MODE_NONE,
@@ -22,13 +30,14 @@ PARAM_CLASSDEFINE(PARAM_VB_CFG_S,VBPOOL,CTX,VB)[] = {
 PARAM_SYS_CFG_S  g_stSysCtx = {
     .u8VbPoolCnt = 1,
     .u8ViCnt = 2,
-    .stVIVPSSMode.aenMode[0] = VI_OFFLINE_VPSS_ONLINE,
-    .stVIVPSSMode.aenMode[1] = VI_OFFLINE_VPSS_ONLINE,
+    .stVIVPSSMode.aenMode[0] = VI_OFFLINE_VPSS_OFFLINE,
+    .stVIVPSSMode.aenMode[1] = VI_OFFLINE_VPSS_OFFLINE,
+    .stVIVPSSMode.aenMode[2] = VI_OFFLINE_VPSS_OFFLINE,
     .stVPSSMode.ViPipe[0] = 0,
     .stVPSSMode.aenInput[0] = VPSS_INPUT_MEM,
     .stVPSSMode.enMode = VPSS_MODE_DUAL,
     .stVPSSMode.ViPipe[1] = 0,
-    .stVPSSMode.aenInput[1] = VPSS_INPUT_ISP,
+    .stVPSSMode.aenInput[1] = VPSS_INPUT_MEM,
     .pstVbPool = PARAM_CLASS(VBPOOL,CTX,VB),
 };
 
